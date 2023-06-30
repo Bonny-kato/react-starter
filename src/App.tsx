513868a102232a5fb8094cfc1d386966a402e4e5
Black <86022898+Bonny-kato@users.noreply.github.com>
2023-07-01 01:58:36 +0300

    feat(config): Add React Query and UI improvements to App component
    Integrated the QueryClient and QueryClientProvider from React Query into the App component to enhance data fetching capabilities and allow for state management. Established ReactQueryDevtools for easier troubleshooting. Imported AppRoutes and Toaster for improved navigation and user notifications. These changes provide a more robust and user-friendly application experience.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import AppRoutes from "@/routes";
+import { Toaster } from "react-hot-toast";
+
+export const queryClient = new QueryClient();
+
 function App() {
-    return <div className={"h-screen bg-black text-white"}></div>;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <AppRoutes />
+            <Toaster position={"top-center"} />
+            <ReactQueryDevtools
+                initialIsOpen={false}
+                position={"bottom-right"}
+            />
+        </QueryClientProvider>
+    );
 }
 
 export default App;
